ea1d92e47c354c26abcd26a34c15050cf74c836d
passelle <pascal.selle@non.agilent.com>
2017-05-24 13:28:07 +0200

    started with project_read

--- src/project.h
+++ src/project.h
@@ -20,7 +20,7 @@ typedef struct
 
 BOOL project_exists(char const * const);
 BOOL project_create(project const * const, char const * const);
-BOOL project_read(project * * const, char const * const);
+BOOL project_read(project * const, char const * const);
 BOOL project_uptar(cmplx const * const, char const * const);
 
 #endif
--- src/project_read.c
+++ src/project_read.c
@@ -1,1 +1,29 @@
+#include "project.h"
 
+#include "utils/file.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+BOOL project_read(project * const p, char const * const path)
+{
+	char *cfg_path;
+	FILE *f;
+
+	cfg_path = path_combine(path, CFG_NAME);
+
+	f = fopen(cfg_path, "r");
+
+	if (!f)
+	{
+		fptintf(stderr, "unable to open %s\n", cfg_path);
+		free(cfg_path);
+		return FALSE;
+	}
+
+	
+
+	fclose(f);
+	free(cfg_path);
+	return TRUE;
+}
